a56e393827c33396419b256b0bce21fa8cf5a905
dustinchang <dustinlchang@gmail.com>
2016-01-27 22:26:05 -0800

    after lab3 polymorphism

--- Raytracer/ImageKit/main.cpp
+++ Raytracer/ImageKit/main.cpp
@@ -1,52 +1,30 @@
 #include "Image.h"
-//#include "GLFW\include\glew.h" For Windows
 #include "raytrace.h"
-
 
 int main(int, char**){
 
+    /*
     Image image(512, 512);
 
-    for (int i = 0; i < 512; ++ i)
-    {
-        for (int j = 0; j < 512; ++ j)
+        for (int i = 0; i < 512; ++ i)
         {
-            Pixel px;
-            //Set the pixel with some colour....
-            px.R = i / 2;
-            px.G = j / 2;
-            px.B = (i + j) / 4;
-            px.A = 255;
+            for (int j = 0; j < 512; ++ j)
+            {
+                Pixel px;
+                //Set the pixel with some colour....
+                px.R = i / 2;
+                px.G = j / 2;
+                px.B = (i + j) / 4;
+                px.A = 255;
 
-            image(i, j) = px;
+                image(i, j) = px;
+            }
         }
-    }
 
-    RayTraceSphere(&image);
     image.show("GLFW3+Libpng Image Window Demo");
-    image.save("/Users/dustin/Desktop/demo.png");
+    image.save("demo.png");*/
 
-    Pixel Black(0, 0, 0);
-    Pixel White(255, 255, 255);
-    int CheckerSize = 128;
-
-    for (int i = 0; i<512; ++i) {
-        for (int j = 0; j<512; ++j) {
-            bool BlackOrWhite;
-            //SOME CALCS
-            int iOffset = i - (i / CheckerSize) * CheckerSize;
-            int jOffset = j - (j / CheckerSize) * CheckerSize;
-
-            int ibit = iOffset / (CheckerSize / 2);
-            int jbit = jOffset / (CheckerSize / 2);
-
-            BlackOrWhite = ibit ^ jbit;
-
-            if (BlackOrWhite) image(i, j) = Black;
-            else image(i, j) = White;
-        }
-    }
-
-    image.show("Checker Board Example");
-    image.save("/Users/dustin/Desktop/CheckerBoard.png");
+	Image raytraceImage(512, 512);
+    RayTraceSphere(&raytraceImage);
+    raytraceImage.show("Ray Tracing Sphere Demo");
 }
--- Raytracer/ImageKit/raytrace.h
+++ Raytracer/ImageKit/raytrace.h
@@ -1,116 +1,100 @@
 //This is a simple ray tracing example showing how to use the Image 
 #pragma once
 #include "Image.h"
-#include <math.h>
-
-struct Vector3
-{
-	float x, y, z;
-	Vector3(float x_in, float y_in, float z_in)
-	{
-		x = x_in;
-		y = y_in;
-		z = z_in;
-	}
-};
-
-Vector3 Add(Vector3 a, Vector3 b) {	return Vector3(a.x + b.x, a.y + b.y, a.z + b.z); }
-Vector3 Minus(Vector3 a, Vector3 b) { return Vector3(a.x - b.x, a.y - b.y, a.z - b.z); }
-Vector3 MultiplyScalar(Vector3 a, float b) { return Vector3(a.x * b, a.y * b, a.z * b); }
-float DotProduct(Vector3 a, Vector3 b) { return a.x * b.x + a.y * b.y + a.z * b.z; }
-Vector3 Normalize(Vector3 a)
-{
-	float length = a.x * a.x + a.y * a.y + a.z * a.z;
-	length = sqrt(length);
-	return Vector3(a.x / length, a.y / length, a.z / length);
-}
+#include <vector>
+#include "sphere.h"
+#include "floor.h"
+#include <iostream>
 
 void SetColor(Pixel & px, Vector3 CalculatedColor)
 {
-	if (CalculatedColor.x < 0) px.R = 0;
-	else if (CalculatedColor.x > 255) px.R = 255;
-	else px.R = (unsigned char)CalculatedColor.x;
+    if (CalculatedColor.x < 0) px.R = 0;
+    else if (CalculatedColor.x > 255) px.R = 255;
+    else px.R = (unsigned char)CalculatedColor.x;
 
-	if (CalculatedColor.y < 0) px.G = 0;
-	else if (CalculatedColor.y > 255) px.G = 255;
-	else px.G = (unsigned char)CalculatedColor.y;
+    if (CalculatedColor.y < 0) px.G = 0;
+    else if (CalculatedColor.y > 255) px.G = 255;
+    else px.G = (unsigned char)CalculatedColor.y;
 
-	if (CalculatedColor.z < 0) px.B = 0;
-	else if (CalculatedColor.z > 255) px.B = 255;
-	else px.B = (unsigned char)CalculatedColor.z;
+    if (CalculatedColor.z < 0) px.B = 0;
+    else if (CalculatedColor.z > 255) px.B = 255;
+    else px.B = (unsigned char)CalculatedColor.z;
 
-	px.A = 255;
+    px.A = 255;
+}
+
+Vector3 AmbientColour(20, 20, 20);
+Vector3 DiffuseColour(250, 250, 250);
+Vector3 BackgroundColor(8, 8, 8);
+Vector3 Light(128, 128, 0);
+//******WARNING: THIS CODE MAKES EVERYTHING IN THE SCENE LOOKS THE SAME****
+//SIMPLY COPY THIS CODE DOES NOT MEET THE REQUIREMENT FOR ASSIGNMENT 1
+//YOU ARE EXPECTED TO HAVE DIFFERENT SURFACE MATERIALS FOR DIFFERENT OBJECTS
+//IN YOUR SCENE.
+Pixel DiffuseShade(Vector3 Surface, Vector3 Normal)
+{
+    //These colours will give the sphere a red appearance
+    Pixel shade;
+    Vector3 LightVector = Minus(Light, Surface);
+    LightVector = Normalize(LightVector);
+    float DiffuseTerm = DotProduct(LightVector, Normal);
+    Vector3 PixelColour = AmbientColour;
+    if (DiffuseTerm > 0)
+    {
+        Vector3 PixelDiffuseColour = MultiplyScalar(DiffuseColour, DiffuseTerm);
+        PixelColour = Add(PixelColour, PixelDiffuseColour);
+    }
+    SetColor(shade, PixelColour);
+    return shade;
 }
 
 void RayTraceSphere(Image * pImage)
 {
-	//The Image is assumed to be 512 x 512
-	//We put the camera at 256, 256, -200
-	//and the sphere at 256, 256, 450, with radius 400.
-	//The light will be at 128, 128, 0,
-	//to give a bright spot on the upper-left corner of the sphere
-	
-	Vector3 Camera(256, 256, -200);
-	Vector3 SphereCenter(256, 256, 450);
-	float SphereRadius = 400;
-	Vector3 Light(128, 128, 0);
+    std::vector<Object *> pObjectList;
+    Floor floor;
+    Sphere sphere(Vector3(256, 256, 450), //center
+                  250);//radius
+    pObjectList.push_back(&floor);
+    pObjectList.push_back(&sphere);
 
-	//These light colours will give the sphere a red appearance
-	Vector3 AmbientColour(20, 10, 10);
-	Vector3 DiffuseColour(255, 128, 128);
-	Vector3 BackgroundColor(64, 64, 128);	
-	
-	for (int i = 0; i < 512; ++ i)
+    Vector3 Camera(256, 256, -200);	//Was z=-400
+
+    for (int i = 0; i < 512; ++ i)
 		for (int j = 0; j < 512; ++j)
 		{
-			Pixel px;
-			Vector3 PixelPosition((float)i, (float)j, 0);
+            //Set up the ray we're tracing: R = O + tD;
+            Pixel px;
+            Vector3 PixelPosition((float)i, (float)j, 0);
 			Vector3 Direction = Minus(PixelPosition, Camera);
 			Direction = Normalize(Direction);
 
-			//Ray-sphere intersection, please refer to the textbook
-			//for the notations
-			Vector3 OC = Minus(Camera, SphereCenter);			
-			float D_Dot_OC = DotProduct(Direction, OC);
-			float RadiusSquare = SphereRadius * SphereRadius;
-			float OCSquare = DotProduct(OC, OC);
-            float discriminant = RadiusSquare - OCSquare + D_Dot_OC * D_Dot_OC;
-			float t = -1;
+            float t_min = 999999;
+            Vector3 Normal_min;
+            bool HasIntersection = false;
 
-			if (discriminant > 0){
-				float discRoot = sqrt(discriminant);
-				float B = (-1) * DotProduct(Direction, OC);
-                float t1 = B + discRoot;
-                float t2 = B - discRoot;
-				//determine which one is the real intersection point
+            //Intersect with the list of objects
+            for (int k = 0; k < pObjectList.size(); ++ k)
+            {
+                float t;
+                Vector3 normal;
+                bool DoesIntersect = pObjectList[k]->Intersect(Camera, Direction,
+                                                             &t, &normal);
+                if (DoesIntersect)
+                {
+                    HasIntersection = true;
+                    if (t_min > t)
+                    {
+                        t_min = t;
+                        Normal_min = normal;
+                    }
+                }
+            }
 
-				if (t1 > 0){
-					if (t2 > 0){
-						t = t2;
-					}
-					else { //Camera is in the sphere
-						t = t1;
-					}
-				}
-			}//if discrimiant > 0
-
-			if (t > 0)
-			{
-				Vector3 Intersection = MultiplyScalar(Direction, t);
+            if (HasIntersection)
+			{              
+                Vector3 Intersection = MultiplyScalar(Direction, t_min);
 				Intersection = Add(Intersection, Camera);
-				Vector3 Normal = Minus(Intersection, SphereCenter);
-				Normal = Normalize(Normal);
-				Vector3 LightVector = Minus(Light, Intersection);
-				LightVector = Normalize(LightVector);
-
-				float DiffuseTerm = DotProduct(LightVector, Normal);
-				Vector3 PixelColour = AmbientColour;
-				if (DiffuseTerm > 0)
-				{
-					Vector3 PixelDiffuseColour = MultiplyScalar(DiffuseColour, DiffuseTerm);
-					PixelColour = Add(PixelColour, PixelDiffuseColour);
-				}
-				SetColor(px, PixelColour);
+                px = DiffuseShade(Intersection, Normal_min);
 			}//if t > 0
 			else //No Intersection, set background colour
 			{
