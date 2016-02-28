4ddd47d0883b5ca5df87daa83f0a2e0aec1e43fd
dustinchang <dustinlchang@gmail.com>
2016-02-27 21:35:02 -0800

    adding new ImageKit305 for assign2

---
+++ openGL/ImageKit_305/main.cpp
@@ -0,0 +1,54 @@
+#include "Canvas.h"
+#include <math.h>
+
+unsigned int width = 512;
+unsigned int height = 512;
+
+Canvas canvas;
+
+void InitializeGL()
+{
+
+}
+
+void MouseMove(double x, double y)
+{
+
+}
+
+void MouseButton(MouseButtons mouseButton, bool press)
+{
+
+}
+
+void KeyPress(char keychar)
+{
+
+}
+
+void OnPaint()
+{
+    
+}
+
+void OnTimer()
+{
+
+}
+
+int main(int, char **){
+    //Link the call backs
+    canvas.SetMouseMove(MouseMove);
+    canvas.SetMouseButton(MouseButton);
+    canvas.SetKeyPress(KeyPress);
+    canvas.SetOnPaint(OnPaint);
+    canvas.SetTimer(0.05, OnTimer);
+    //Show Window
+    canvas.Initialize(width, height, "OpenGL Intro Demo");
+    //Do our initialization
+    InitializeGL();
+    canvas.Show();
+
+    return 0;
+}
+
