a1f869ae919b7f6465e15d9009e229677aaebfde
dustinchang <dustinlchang@gmail.com>
2016-02-28 16:16:23 -0800

    got eigen Matrix working

--- openGL/ImageKit_305/main.cpp
+++ openGL/ImageKit_305/main.cpp
@@ -1,5 +1,9 @@
 #include "Canvas.h"
 #include <math.h>
+#include <iostream>
+#include "Eigen/Dense"
+using namespace std;
+using namespace Eigen;
 
 unsigned int width = 512;
 unsigned int height = 512;
@@ -10,9 +14,15 @@ bool leftButtonPressed = false;
 //Rotation
 bool rotateCW = true; //Whether we're rotating clockwise or not
 float rotateAngle = 0;
-float rotateSpeed = 0.07;    //was 0.2
+float rotateSpeed = 0.001;    //was 0.2
 
 Canvas canvas;
+
+//Use Eigen to produce the Perspective Matrix
+//n = dist to near plane
+//f = dist to far plane
+Matrix4d perspecMat;
+
 
 struct Vector2 {
   float x, y;
@@ -20,6 +30,14 @@ struct Vector2 {
     x = x_in; y = y_in;
   }
   Vector2() {x = y = 0;}
+};
+
+struct Vector3 {
+  float x, y, z;
+  Vector3(float x_in, float y_in, float z_in) {
+    x = x_in; y = y_in; z = z_in;
+  }
+  Vector3() {x = y = z =0;}
 };
 
 //To create square now
@@ -63,11 +81,17 @@ void OnPaint()
     for(int i = 0; i < 4; ++ i) {
       Vector2 startPoint;
       //If do nothing at all, but we want to rotate that point so we add all this formula from tutorial/textbook
+      //cout << "rotateAngle=" << rotateAngle << endl;
       startPoint.x = SquareVertices[i].x * cos(rotateAngle) - SquareVertices[i].y * sin(rotateAngle); //Change to -1 if you want to expand
+      //cout << "rotateAngle =" << rotateAngle << endl;
       startPoint.y = SquareVertices[i].x * sin(rotateAngle) + SquareVertices[i].y * cos(rotateAngle);
       Vector2 endPoint;
       endPoint.x = SquareVertices[i + 1].x * cos(rotateAngle) - SquareVertices[i + 1].y * sin(rotateAngle);
       endPoint.y = SquareVertices[i + 1].x * sin(rotateAngle) + SquareVertices[i + 1].y * cos(rotateAngle);
+      //cout << "startPoint x =" << startPoint.x << endl;
+      //cout << "startPoint y =" << startPoint.y << endl;
+      //cout << "endPoint x =" << endPoint.x << endl;
+      //cout << "endPoint y =" << endPoint.y << endl;
       canvas.AddLine(startPoint.x, startPoint.y, endPoint.x, endPoint.y);
     }
 }
@@ -81,13 +105,18 @@ void OnTimer()
 }
 
 int main(int, char **){
+    perspecMat <<   1, 0, 0, 0,
+              0, 1, 0, 0,
+              0, 1, 0, 0,  //0, 0, (n+f)/n, -f,
+              0, 1, 0, 0;  //0, 0, (1/n), 0;
+
   //Init of square coordinate
   SquareVertices.push_back(Vector2(-0.5, -0.5));
   SquareVertices.push_back(Vector2(0.5, -0.5));
   SquareVertices.push_back(Vector2(0.5, 0.5));
   SquareVertices.push_back(Vector2(-0.5, 0.5));
   SquareVertices.push_back(Vector2(-0.5, -0.5));
-  
+
     //Link the call backs
     canvas.SetMouseMove(MouseMove);
     canvas.SetMouseButton(MouseButton);
