df59099781f5f6548a4340566273121ed4515332
dustinchang <dustinlchang@gmail.com>
2016-04-09 19:16:20 -0700

    still errors and still working, just dont want to lose work

--- Raytracer/ImageKit/raytrace.h
+++ Raytracer/ImageKit/raytrace.h
@@ -2,14 +2,22 @@
 #pragma once
 #include <vector>
 #include <iostream>
+#include <algorithm>
 #include "Image.h"
 #include "light.h"
 #include "object.h"
 #include "plane.h"
 #include "sphere.h"
+using namespace std;
+
+#define LIGHTS 2
+#define PHONGPOW 2
+#define SPHERES 4
 
 std::vector<Object *> ObjVec;
-std::vector<Light *> lightsVec;
+std::vector<Light *> LightsVec;
+
+int refl_obj;
 
 //16 rays/px anit-aliasing, done for each 2x2 section
 Pixel Sect1, Sect2, Sect3, Sect4;
@@ -79,8 +87,152 @@ bool ifIntersection(Vector3 *Intersection, bool HasIntersection, Vector3 Directi
   }
 }
 
+//Return lighter color
+Vector3 LighterCol(Vector3 MaxCol, Vector3 OutCol) {
+  int mc = MaxCol.x + MaxCol.y + MaxCol.z;
+  int oc = OutCol.x + OutCol.y + OutCol.z;
+  if(mc < oc) { return OutCol; }
+  else { return MaxCol; }
+}
+
+//Add Pixel colors and prevent going higher than 255
+Pixel AddColors(Pixel px, Pixel refl){
+    int x,y,z;
+    Pixel a;
+    x = px.R + refl.R;
+    y = px.G + refl.G;
+    z = px.B + refl.B;
+    if(x > 255) x = 255;
+    if(y > 255) y = 255;
+    if(z > 255) z = 255;
+    a.R = x; a.G = y; a.B = z;
+    return a;
+}
+
+//Returns Avg Color of 4 Pixels
+Pixel AveragePixel(Pixel a, Pixel b, Pixel c, Pixel d){
+    Pixel x;
+    x.R = (a.R + b.R + c.R + d.R )/4;
+    x.G = (a.G + b.G + c.G + d.G )/4;
+    x.B = (a.B + b.B + c.B + d.B )/4;
+    return x;
+}
+
+Pixel Shade(Vector3 SurfCol, Vector3 Normal, Vector3 Direction, Vector3 Intersection, float LightIntensity) {
+  Pixel px;
+  Vector3 MaxCol(0,0,0);
+  bool inShadow = false;
+  bool inShade[LIGHTS] = {0};
+
+  for(int i=0; i<LightsVec.size(); i++) {
+    //Labertain
+    Vector3 L = Normalize(Minus(LightsVec[i]->Point, Intersection));
+    cout << "DotProduct=" << DotProduct(Normal, L) << endl;
+    float Intensity_Max = LightIntensity * max((float)0, DotProduct(Normal, L));
+    Vector3 OutCol = MultiplyScalar(SurfCol, Intensity_Max);
+
+    //Phong part
+    Vector3 H = Normalize(Add(L, Direction));
+    float SMax = max((float)0, DotProduct(Normal, H));
+    float temp = SMax;
+    for(int j=0; j<PHONGPOW; j++) { SMax += temp; }
+    SMax *= LightIntensity;
+    OutCol = Add(OutCol, MultiplyScalar(Vector3(200,200,200), SMax));
+    SetColor(px, OutCol);
+    MaxCol = LighterCol(MaxCol, OutCol);
+
+    //Intersection pt adjustments to avoid black plague
+    Intersection = Add(Intersection, MultiplyScalar(L, 0.2));
+
+    //Shadow
+    float t_min = 999999;
+    bool HasIntersection = false;
+    //Intersection with list of objects
+    for(int k=0; k<ObjVec.size() ; k++) {
+      float t;
+      Vector3 norm;
+      Vector3 col;
+      bool DoesIntersect = ObjVec[k]->Intersect(Intersection, L, &t, &norm, &col);
+      if(DoesIntersect) {
+        inShade[i] = true;
+        inShadow = true;
+        break;
+      }
+    }//End of k for
+  }//End of i for
+  if(inShadow) {
+    for(int m=0; m<LIGHTS; m++) {
+      if(inShade[m]) MaxCol = MultiplyScalar(MaxCol, 0.5);
+    }
+  }
+  SetColor(px, MaxCol);
+  return px;
+}//End of Shade
+
+Pixel DetermineRay(Vector3 StartPoint, Vector3 Direction, float LightIntensity, int j) {
+  Pixel px;
+  float t_min = 999999;
+  Vector3 Normal_min;
+  Vector3 colors;
+  bool HasIntersection = false;
+  int obj_hit;
+
+  //Intersect with the list of objects
+  for (int k = 0; k < ObjVec.size(); ++ k) {
+    float t;
+    Vector3 normal;
+    Vector3 col;
+    bool DoesIntersect = ObjVec[k]->Intersect(StartPoint, Direction, &t, &normal, &col);
+    //Determine the min t if an intersection
+    if (DoesIntersect)
+    {
+        HasIntersection = true;
+        if (t_min > t)
+        {
+            t_min = t;
+            Normal_min = normal;
+            colors = col;
+            obj_hit = k;
+        }
+    }
+  }
+  if (HasIntersection) {
+    Vector3 Intersection = MultiplyScalar(Direction, t_min);
+    Intersection = Add(Intersection, StartPoint);
+    px = Shade(colors, Normal_min, Direction, Intersection, LightIntensity);
+
+    //Make chosen object reflective
+    if(obj_hit == refl_obj ){
+        //r = d - 2(d . n)n
+        Vector3 N = Normal_min;
+        float TwoD_Dot_N = DotProduct(Direction,N) * 2;
+        Vector3 N_Time = MultiplyScalar(N, TwoD_Dot_N);
+        Vector3 ReflRay = Minus(Direction, N_Time);
+        ReflRay = Normalize(ReflRay);
+        Pixel refl = DetermineRay(Intersection, ReflRay, 0.9, j);
+        px = AddColors(refl,px);
+    }
+    //If Hit bottom/floor plane
+    if(obj_hit == 4){
+        //Give stripe texture
+        Vector3 stripecolor(120, 120, 120);
+        Pixel h;
+        if(sin(M_PI*j/8)>0){
+            SetColor(h, stripecolor);
+            //px = MinusColors(px,h);
+        }
+    }
+  }//if t > 0
+  else //No Intersection, set background colour
+  {
+    SetColor(px, BackgroundColor);
+  }
+  return px;
+}//End of DetermineRay
+
 //Add the directions
-void addDirections(Vector3 Camera, Vector3 D1, Vector3 D2, Vector3 D3, Vector3 D4, int sectNum, float LightIntensity, ) {
+void addDirections(Vector3 Camera, Vector3 PixelPosition, Vector3 D1, Vector3 D2, Vector3 D3, Vector3 D4, int sectNum, float LightIntensity, int j) {
+  Vector3 Direction1, Direction2, Direction3, Direction4;
   //Calculates the sections
   Direction1 = Add(PixelPosition,D1);
   Direction1 = Minus(Direction1,Camera);
@@ -97,13 +249,20 @@ void addDirections(Vector3 Camera, Vector3 D1, Vector3 D2, Vector3 D3, Vector3 D
 
   switch (sectNum) {
     case 1:
-      Sect1 = AveragePixel(ComputeRay(Camera, Direction1, LightIntensity, j),ComputeRay(Camera, Direction2, LightIntensity, j), ComputeRay(Camera, Direction3, LightIntensity, j),ComputeRay(Camera, Direction4, LightIntensity, j));
+      Sect1 = AveragePixel(DetermineRay(Camera, Direction1, LightIntensity, j),DetermineRay(Camera, Direction2, LightIntensity, j), DetermineRay(Camera, Direction3, LightIntensity, j),DetermineRay(Camera, Direction4, LightIntensity, j));
       break;
     case 2:
-      Sect2 =
+      Sect2 = AveragePixel(DetermineRay(Camera, Direction1, LightIntensity, j),DetermineRay(Camera, Direction2, LightIntensity, j), DetermineRay(Camera, Direction3, LightIntensity, j),DetermineRay(Camera, Direction4, LightIntensity, j));
       break;
+    case 3:
+      Sect3 = AveragePixel(DetermineRay(Camera, Direction1, LightIntensity, j),DetermineRay(Camera, Direction2, LightIntensity, j), DetermineRay(Camera, Direction3, LightIntensity, j),DetermineRay(Camera, Direction4, LightIntensity, j));
+      break;
+    case 4:
+      Sect4 = AveragePixel(DetermineRay(Camera, Direction1, LightIntensity, j),DetermineRay(Camera, Direction2, LightIntensity, j), DetermineRay(Camera, Direction3, LightIntensity, j),DetermineRay(Camera, Direction4, LightIntensity, j));
+      break;
+      default:
+        cout << "Shouldn't reach here" << endl;
   }
-
 }
 
 
@@ -112,9 +271,9 @@ void RayTrace_Image(Image * pImage)
 {
   //Creation of Lights
   Light L1(Vector3(120,120,0));
-  LightList.push_back(&L1);
+  LightsVec.push_back(&L1);
   Light L2(Vector3(200,256,0));
-  LightList.push_back(&L2);
+  LightsVec.push_back(&L2);
   float LightIntensity = 1.0;
   /////Creation of objects
   ///std::vector<Object *> pObjectList;
@@ -125,6 +284,18 @@ void RayTrace_Image(Image * pImage)
 
   //Put planes into ObjVec
   ObjVec.push_back(&bottom);
+
+  refl_obj = 0;
+
+  //Cycle through image pixels
+  for (int i = 0; i < 512; ++ i)
+    for (int j = 0; j < 512; ++j)
+    {
+      Vector3 PixelPosition((float)i, (float)j, 0);
+      addDirections(Camera, PixelPosition, Vector3(-0.375, 0.375, 0))
+
+    }
+  }
 
 
 
@@ -160,12 +331,13 @@ void RayTrace_Image(Image * pImage)
 
 
 
+/*
+
     //Cycle through image pixels
     for (int i = 0; i < 512; ++ i)
         for (int j = 0; j < 512; ++j)
         {
             //Set up the ray we're tracing: R = O + tD;
-            Pixel px;
             Vector3 PixelPosition((float)i, (float)j, 0);
 			      //Vector3 Direction = Minus(PixelPosition, Camera);
 			      //Direction = Normalize(Direction);
@@ -188,10 +360,7 @@ void RayTrace_Image(Image * pImage)
 
 
 
-            float t_min = 999999;
-            Vector3 Normal_min;
-            Vector3 colors;
-            bool HasIntersection = false;
+
 
             //Intersect with the list of objects
             for (int k = 0; k < pObjectList.size(); ++ k)
@@ -228,7 +397,11 @@ void RayTrace_Image(Image * pImage)
               SetColor(px, BackgroundColor);
             }
 
+            */
+
       //Set the pixel image
-			(*pImage)(i, j) = px;
+			///(*pImage)(i, j) = px;
+      //Get Average of the 4 section colours
+      (*pImage)(i, 511-j) = AveragePixel(Section1,Section2, Section3, Section4);
 		}
 }
